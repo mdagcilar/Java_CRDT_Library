e3e10ca9351f5a42b8245abfef3c849b27e200e9
mdagcilar <md3g11@soton.ac.uk>
2016-03-08 20:27:02 +0000

    simple testing for GSet and TwoPhaseSet

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,5 @@
 import crdt.Counters.PNCounter;
+import crdt.sets.TwoPhaseSet;
 
 /***
  create new CRDT counter type
@@ -10,7 +11,8 @@ public class main {
     public static void main(String[] args){
 
         main main1 = new main();
-        main1.testingPNCounter();
+        //main1.testingPNCounter();
+        main1.testingTwoPhaseSet();
     }
 
 
@@ -31,6 +33,30 @@ public class main {
 
         replica1.merge( replica2 );
         System.out.println("\nReplica1 value after merge = " + replica1.value());
+    }
+
+    /*
+    A method for testing the expected output of a TwoPhaseSet.
+     */
+    public void testingTwoPhaseSet()
+    {
+        TwoPhaseSet<String> replica1 = new TwoPhaseSet<String>();
+
+        replica1.add("a");
+        replica1.add("b");
+
+
+        TwoPhaseSet<String> replica2 = replica1.copy();
+        replica2.remove("b");
+        replica2.add("c");
+
+        replica1.merge(replica2);
+
+        System.out.println("replica1 elements = " + replica1.get());
+
+
+
+
     }
 
 }
